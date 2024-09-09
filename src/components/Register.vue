67d4b7789e5c8c7e3dc00642bb14e8fb85a2457c
Urooj Khan <khan.9386@apac.teleperformance.com>
2024-09-10 01:01:08 +0530

    adding with convex

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,56 +1,39 @@
 <template>
   <section class="grid grid-cols-2 min-h-dvh">
     <div class="max-sm:hidden right"></div>
-    <div
-      class="w-full h-full grid content-center justify-items-center lightyellow"
-    >
+    <div class="w-full h-full grid content-center justify-items-center lightyellow">
       <h1 class="text-3xl mb-2 font-semibold">Social Media</h1>
       <h1 class="text-xl mb-8">Create an account</h1>
-      <form @submit.prevent="() => register(user)" class="grid gap-4">
+      <form @submit.prevent="() => { }" class="grid gap-4">
         <div class="form-group">
           <label for="">Name</label>
-          <input type="text" v-model="user.name" placeholder=" " required />
+          <input type="text" placeholder=" " required />
         </div>
         <div class="form-group">
           <label for="">Username</label>
-          <input type="text" v-model="user.username" placeholder=" " required />
+          <input type="text" placeholder=" " required />
         </div>
         <div class="form-group">
           <label for="">Email</label>
-          <input type="text" v-model="user.email" placeholder=" " required />
+          <input type="text" placeholder=" " required />
         </div>
         <div class="form-group">
           <label for="">Password</label>
-          <input
-            type="password"
-            v-model.trim="user.password"
-            name="password"
-            placeholder=" "
-            required
-          />
+          <input type="password" name="password" placeholder=" " required />
         </div>
         <div class="form-group">
           <label for="">Confirm Password</label>
-          <input
-            type="password"
-            v-model.trim="cpassword"
-            placeholder=" "
-            required
-          />
+          <input type="password" v-model.trim="cpassword" placeholder=" " required />
         </div>
-        <button :disabled="!isMatching">Register</button>
-        <RouterLink to="/auth/login" class="text-sm text-black/70"
-          >Already have an account?
-          <span class="font-semibold text-black">Log in</span></RouterLink
-        >
+        <button>Register</button>
+        <RouterLink to="/auth/login" class="text-sm text-black/70">Already have an account?
+          <span class="font-semibold text-black">Log in</span>
+        </RouterLink>
         <div class="text-center text-black/70 my-4">or</div>
         <div
-          class="flex items-center gap-4 justify-center border border-gray-400 p-2.5 text-center text-sm rounded-md cursor-pointer"
-        >
-          <img
-            src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png"
-            class="size-6"
-          />
+          class="flex items-center gap-4 justify-center border border-gray-400 p-2.5 text-center text-sm rounded-md cursor-pointer">
+          <img src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png"
+            class="size-6" />
           Continue with google
         </div>
       </form>
@@ -59,24 +42,11 @@
 </template>
 
 <script setup lang="ts">
-import { register } from "../store/user.store";
-import { RegisterUser } from "../interfaces/user.interface";
-import { ref, computed, reactive } from "vue";
-const user = reactive<RegisterUser>({
-  name: "",
-  username: "",
-  email: "",
-  password: "",
-});
+import { ref } from "vue";
 
 const cpassword = ref("");
-const isMatching = computed(() => {
-  if (user.password.length && cpassword.value.length)
-    return user.password == cpassword.value;
-});
 </script>
 
 <style scoped>
 @import "../css/auth.css";
 </style>
-../store/user2.store
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,43 +1,31 @@
 <template>
-  <nav
-    :class="{ opened: isNavOpened }"
-    class="h-dvh min-w-80 p-8 flex flex-col gap-8 transition-all duration-200 z-10"
-  >
+  <nav :class="{ opened: isNavOpened }" class="h-dvh min-w-80 p-8 flex flex-col gap-8 transition-all duration-200 z-10">
     <section class="grid justify-items-center">
       <img class="size-16 rounded-full" src="http://picsum.photos/100.webp" />
       <div class="font-[600]">urooj</div>
       <div class="text-xs text-gray-400">@uroo</div>
     </section>
     <section class="grid gap-2">
-      <RouterLink
-        v-for="link in routes"
-        :to="link.path"
-        class="py-3 px-4 rounded-xl font-semibold active flex items-center gap-2 transition-all duration-300 hover:bg-black hover:text-white"
-      >
+      <RouterLink v-for="link in routes" :to="link.path"
+        class="py-3 px-4 rounded-xl font-semibold active flex items-center gap-2 transition-all duration-300 hover:bg-black hover:text-white">
         <Icon :icon="link.icon" class="text-lg" />
         {{ link.name }}
 
-        <div
-          class="size-6 ml-auto text-xs bg-black text-white rounded-full grid place-items-center transition-colors"
-          v-if="link?.notification"
-        >
+        <div class="size-6 ml-auto text-xs bg-black text-white rounded-full grid place-items-center transition-colors"
+          v-if="link?.notification">
           {{ link.notification }}
         </div>
       </RouterLink>
     </section>
 
-    <button
-      @click="userStore.logout"
-      class="mt-auto py-3 px-4 rounded-xl font-semibold active flex items-center gap-2 transition-all duration-300 hover:bg-black hover:text-white"
-    >
+    <button @click=""
+      class="mt-auto py-3 px-4 rounded-xl font-semibold active flex items-center gap-2 transition-all duration-300 hover:bg-black hover:text-white">
       <Icon icon="solar:logout-3-bold" class="text-lg" />
       Logout
     </button>
-    <div
-      @click="isNavOpened = false"
+    <div @click="isNavOpened = false"
       class="md:hidden size-10 bg-white border border-gray-300 cursor-pointer grid place-items-center rounded-full fixed top-1/2 -translate-y-1/2 left-64 opacity-0 transition-all duration-200 delay-200 hover:bg-black hover:text-white"
-      :class="{ 'translate-x-[72px] opacity-100': isNavOpened }"
-    >
+      :class="{ 'translate-x-[72px] opacity-100': isNavOpened }">
       <Icon icon="humbleicons:times" class="text-xl" />
     </div>
   </nav>
@@ -95,7 +83,7 @@ const routes = [
   }
 
   nav.opened {
-    @apply translate-x-0 shadow-[4px_0_16px_4px_rgba(0,0,0,0.2)];
+    @apply translate-x-0 shadow-[4px_0_16px_4px_rgba(0, 0, 0, 0.2)];
   }
 }
 </style>
